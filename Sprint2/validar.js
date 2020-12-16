0e9f7987fb7a72dc0aa6acf983585d9b50ab38fd
Cesar Carvajal <acarvajal@uninorte.edu.co>
2020-12-15 22:31:47 -0500

    Proyecto

---
+++ Sprint2/validar.js
@@ -0,0 +1,61 @@
+function validar_Registro()
+{
+    var correo = document.getElementById("correo").value;
+    var usuario = document.getElementById("usuario").value;
+    var clave = document.getElementById("clave").value;
+    var confirClave = document.getElementById("confirClave").value;
+
+    if (correo == "")
+    {
+        alert("Debe digitar el correo.");
+        document.getElementById("correo").focus();
+        return false;
+    } 
+
+    if ((usuario == "") || (usuario.length < 8))
+    {
+        alert("El usuario debe tener mínimo 8 caracteres.");
+        document.getElementById("usuario").focus();
+        return false;
+    }
+        
+    if ((clave == "") || (clave.length < 8))
+    {
+        alert("La clave debe tener mínimo 8 caracteres.");
+        document.getElementById("clave").focus();
+        return false;
+    }
+
+    if ((confirClave == "") || (confirClave.length < 8))
+    {
+        alert("La confirmacion de clave debe tener mínimo 8 caracteres.");
+        document.getElementById("confirClave").focus();
+        return false;
+    }
+    
+    if (confirClave != clave) 
+    {
+        alert("La confirmacion de clave no coincide.");
+        document.getElementById("confirClave").focus();
+        return false;
+    }
+
+    alert("Registro exitoso. Por favor active su cuenta.")
+    return true;
+    
+    
+    
+
+}
+
+function mostrarPassword() 
+{
+    document.getElementById("clave").type = "text";
+}
+
+function ocultarPassword()
+{
+    document.getElementById("clave").type = "password";
+}
+
+
